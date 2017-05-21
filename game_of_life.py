3799ba2b70b0f12ee0220dbdb01fcebe3d19016c
MSurai <simonmerkelbach@outlook.de>
2017-05-21 23:50:48 +0200

    initialize_board and count_neighbors added

--- game_of_life.py
+++ game_of_life.py
@@ -1,5 +1,23 @@
 def main():
-    pass
+    board = initialize_board(5, 5)
+
+
+def initialize_board(width, height):
+    board = [[False for i in range(width)] for j in range(height)]
+    return board
+
+
+def count_neighbors(x, y, board):
+    width = len(board[0])
+    height = len(board)
+    counter = 0
+
+    for yDelta in [-1, 0, 1]:
+        for xDelta in [-1, 0, 1]:
+            if not yDelta == xDelta == 0 and 0 <= y + yDelta < height and 0 <= x + xDelta < width:
+                counter += int(board[y + yDelta][x + xDelta])
+
+    return counter
 
 
 if __name__ == '__main__':
